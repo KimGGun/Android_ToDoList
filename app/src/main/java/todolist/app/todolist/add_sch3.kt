b10f7682c5eeb3702a3e982d37151ac4070a778c
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-07-27 21:31:00 +0900

    일정 추가 3

---
+++ app/src/main/java/todolist/app/todolist/add_sch3.kt
@@ -0,0 +1,12 @@
+package todolist.app.todolist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class add_sch3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_sch3)
+    }
+}
