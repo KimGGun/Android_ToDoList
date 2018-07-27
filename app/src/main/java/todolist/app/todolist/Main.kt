92eaafaeca59cbbc082c84c8673b8d515b8d97ae
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-07-27 21:31:10 +0900

    설정 변경

--- app/src/main/java/todolist/app/todolist/Main.kt
+++ app/src/main/java/todolist/app/todolist/Main.kt
@@ -1,12 +1,19 @@
 package todolist.app.todolist
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Main : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        add_sch_btn.setOnClickListener {
+            val nextIntent = Intent(this, add_sch::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
